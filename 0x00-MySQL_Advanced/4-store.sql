acd80794c755f9969a1b4ea038947ac1c9a0b694
keroti <peterkeroti55@gmail.com>
2023-05-05 02:15:39 +0300

    Store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,8 @@
 -- Create trigger to update item quantity after adding new order
 
-CREATE TRIGGER decrease_item_quantity
+CREATE TRIGGER updated
 AFTER INSERT ON orders
 FOR EACH ROW UPDATE items
 -- NEW has the updated data for table
-
 SET quantity = quantity - NEW.number
-WHERE items.name = NEW.item_name;
+WHERE name = NEW.item_name;
