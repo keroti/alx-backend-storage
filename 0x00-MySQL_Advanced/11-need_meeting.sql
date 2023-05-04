95dc34e25b002fd585fc3c3131d79ff673cd98d2
keroti <peterkeroti55@gmail.com>
2023-05-05 02:46:35 +0300

    No table for meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Creates a view need_meeting that lists all students that have a score under 80 (strict) and no last_meeting or more than 1 month
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND (students.last_meeting IS NULL OR students.last_meeting < ADD_DATE(NOW(), INTERVAL -1 MONTH));
