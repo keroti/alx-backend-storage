47f86aa999652094c06c1719220be6496093c38a
keroti <peterkeroti55@gmail.com>
2023-05-10 00:19:05 +0300

    Search by topic python

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,11 +7,4 @@ Function that returns the list of school having a specific topic
 def schools_by_topic(mongo_collection, topic):
     '''Returns the list of school
     '''
-    topic_search = {
-        'topics': {
-            '$elemMatch': {
-                '$eq': topic,
-            },
-        },
-    }
-    return [school for school in mongo_collection.find(topic_searchl)]
+    return mongo_collection.find({"topics": topic})
