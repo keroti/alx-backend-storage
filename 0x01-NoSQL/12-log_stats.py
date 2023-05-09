e6ac48e252a6aa611b5373d425cb7ff9be449d7f
keroti <peterkeroti55@gmail.com>
2023-05-10 00:41:15 +0300

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -9,20 +9,23 @@ client = MongoClient('mongodb://localhost:27017/')
 database = client.logs
 nginx_collection = database.nginx
 
-# Number of documents in collection
+'''Number of documents in collection
+'''
 num_logs = nginx_collection.count_documents({})
 print(f"{num_logs} logs")
 
-# Number of documents with each method
+'''Number of documents with each method
+'''
 methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for method in methods:
     num_docs = nginx_collection.count_documents({"method": method})
     print(f"    method {method}: {num_docs}")
 
-# Number of documents with method GET and path /status
+'''Number of documents with method GET and path /status
+'''
 num_status_check = nginx_collection.count_documents(
     {"method": "GET", "path": "/status"}
-    )
+)
 print(f"{num_status_check} status check")
 
 if __name__ == '__main__':
