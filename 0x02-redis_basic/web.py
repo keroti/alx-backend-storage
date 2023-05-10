b9c0b40994792c2473d7bb4698ed22f17777935c
keroti <peterkeroti55@gmail.com>
2023-05-10 23:38:58 +0300

    Implement cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,18 +8,31 @@ import requests
 import redis
 from functools import wraps
 
-
+# create Redis client
 redis_client = redis.Redis()
-count = 0
 
 
+def url_count(method: Callable) -> Callable:
+    '''
+    Function for tracking number of times a function is called
+    '''
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(f'{url}')
+        if cached:
+            return cached.decode('utf-8')
+        redis_client.setex(f'{url}, 10, {method(url)}')
+        return method(*args, **kwargs)
+    return wrapper
+
+
+@url_count
 def get_page(url: str) -> str:
     """
     get a page content with cache and tracker
     """
-    redis_client.set(f"cached:{url}", count)
-    redis_client.incr(f"count:{url}")
-    redis_client.setex(f"cached:{url}", 10, redis_client.get(f"cached:{url}"))
     response = requests.get(url)
     return response.text
 
